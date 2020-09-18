bdad2eea65e116ed64659a168dfc673a9c9556c4
Pedro Dias <pedro@dolittle.com>
2020-09-18 15:42:15 +0200

    Initial Commit - Barebone structure

---
+++ src/XmlAssistant/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XmlAssistant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
