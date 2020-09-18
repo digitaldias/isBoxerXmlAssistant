438fa4f77285024e5e7081fa90c31d38944b9e03
Pedro Dias <pedro@dolittle.com>
2020-09-18 15:58:31 +0200

    Introduce syntax help

--- src/XmlAssistant/Program.cs
+++ src/XmlAssistant/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
 
 namespace XmlAssistant
 {
@@ -6,7 +8,25 @@ namespace XmlAssistant
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if(args.Length == 0 || args.Any(a => 
+                             a.Equals("--help", StringComparison.InvariantCultureIgnoreCase) ||
+                             a.Equals("-h", StringComparison.InvariantCultureIgnoreCase))
+              )
+            {
+                ShowHelp();
+            }
+        }
+
+        private static void ShowHelp()
+        {
+            var helpMessage = $"xmlAssistant v{Assembly.GetEntryAssembly().GetName().Version}" + Environment.NewLine +
+                               "---------------------" + Environment.NewLine +
+                               "Syntax:" + Environment.NewLine +
+                               "xmlAssistant <argument> [parameters]" + Environment.NewLine + Environment.NewLine +
+                               "Supported Arguments: " + Environment.NewLine +
+                               "createKeyStrokes 'input string'    : Creates a sequence of MappedKeyActions of type 'KeyStroke' where each character in the input string are a single keystroke " + Environment.NewLine;                               
+                               ;
+              Console.WriteLine(helpMessage);
         }
     }
 }
